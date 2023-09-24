ae1282f5667b1e070fe2cf95e1740978f785f000
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-09-24 20:17:07 +0530

    add new feature- clock

--- Mini-Calendar app/script.js
+++ Mini-Calendar app/script.js
@@ -3,6 +3,7 @@ const date = document.querySelector('#date');
 const day = document.querySelector('#day');
 const month = document.querySelector('#month');
 const year = document.querySelector('#year');
+const clock = document.querySelector('#clock');
 
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
@@ -15,3 +16,8 @@ day.textContent = `${days[time.getDay()]}`;
 month.textContent = `${months[time.getMonth()]}`;
 year.textContent = `${time.getFullYear()}`;
 
+
+setInterval(()=>{
+  const liveTime = new Date();
+  clock.textContent = `${liveTime.toLocaleTimeString()}`;
+},1000)
