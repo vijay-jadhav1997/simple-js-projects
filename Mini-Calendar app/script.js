90a83bc4b0d2327b7e7294dbdece0bf173485121
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-09-24 11:58:21 +0530

    Update to index.html & style.css

--- Mini-Calendar app/script.js
+++ Mini-Calendar app/script.js
@@ -7,6 +7,8 @@ const year = document.querySelector('#year');
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
+
+// using date() function - to get date:
 const time = new Date();
 date.textContent = `${(time.getDate() < 10 ? '0' : '') + time.getDate()}`;
 day.textContent = `${days[time.getDay()]}`;
