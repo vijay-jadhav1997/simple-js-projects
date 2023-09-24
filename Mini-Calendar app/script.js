b7f34d8dc9cab874802f462d253d1b0dd4d4edcc
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-09-24 10:45:00 +0530

    doc : add file- Mini Calendar app

---
+++ Mini-Calendar app/script.js
@@ -0,0 +1,15 @@
+// Elements's references
+const date = document.querySelector('#date');
+const day = document.querySelector('#day');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
+
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+const time = new Date();
+date.textContent = `${(time.getDate() < 10 ? '0' : '') + time.getDate()}`;
+day.textContent = `${days[time.getDay()]}`;
+month.textContent = `${months[time.getMonth()]}`;
+year.textContent = `${time.getFullYear()}`;
+
