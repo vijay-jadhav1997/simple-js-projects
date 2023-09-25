b478c5de0382ac8fba49cd32edb41d795ec374be
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-09-25 14:13:48 +0530

    add new file- digital clock

---
+++ Digital clock/script.js
@@ -0,0 +1,16 @@
+// references
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+// getting time
+
+setInterval(() => {
+  const time = new Date();
+  sec = `${time.getSeconds() < 10 ? '0' : ''}${time.getSeconds()}` 
+  min = `${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}` 
+  hrs = `${time.getHours() < 10 ? '0' : ''}${time.getHours()}` 
+  seconds.innerHTML = `${sec} <span>Sec</span>`; 
+  minutes.innerHTML = `${min} <span>Mins</span>`; 
+  hours.innerHTML = `${hrs} <span>Hours</span>`; 
+},1000);
