6edc284d61ed75df7ed8bc0bf25ac5c7e78ee9ab
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-10-03 10:53:22 +0530

    feat: add new feature
    add new feature- Todo description note, delete button, date-time etc.

--- Todo List App/script.js
+++ Todo List App/script.js
@@ -1,5 +1,8 @@
 // 
 const addBtn = document.querySelector('.add_button');
+let todoTitleText = document.querySelector('#todo_name');
+let todoDateText = document.querySelector('#todo_date');
+let todoDiscriptionText = document.querySelector('#descr_input');
 // console.log(addBtn);
 
 const todoList = document.querySelector('.todo_list')
@@ -9,43 +12,78 @@ let allTodos = [];
 
 addBtn.addEventListener('click' ,(e)=> {
   let todoObject = {};
-  let todoTitleText = document.querySelector('#todo_name').value;
-  let todoDateText = document.querySelector('#todo_date').value;
   // console.log(todoTitle);
   // console.log(todoDate);
-  todoObject.title = `${todoTitleText}`;
-  todoObject.date = `${todoDateText}`;
-  // console.log(todoObject);
+
+  if (todoTitleText.value === '') {
+    alert('Please enter Todo Title...!!')
+  } else if (todoDateText.value === '') {
+    alert('Please select Todo Date-Time...!!')
+  } else {
+    todoObject.title = `${todoTitleText.value}`;
+    todoObject.date = `${todoDateText.value}`;
+    todoObject.discription = `${todoDiscriptionText.value}`;
+    // console.log(todoObject);
+
+    allTodos.push(todoObject);
+    console.log(allTodos);
   
-  allTodos.push(todoObject);
-  console.log(allTodos);
-
-  createTodo();
-
-  todoTitleText = '';
-  todoDateText = '';
+    createTodo();
+  }
+  
 })
 
-
+let deleteBtn = document.querySelector('.delete_Btn');
+let todoBox = document.querySelector('.todo_box');
 // function create todo in todo_list:
 function createTodo() {
   todoList.innerHTML = '';
   allTodos.forEach((todo) => {
     const todoElement = document.createElement('div');
     todoElement.classList.add('todo_box');
-    todoElement.innerHTML = `
-      <h1>${todo.title}</h1>
-      <h3>${todo.date}</h3>
-      <p></p>
-    `;
-    todoList.appendChild(todoElement)
-  })
-  // const todoElement = document.createElement('div');
-  // todoElement.classList.add('todo_box');
-  // todoElement.innerHTML = `
-  //   <h1>${todoTitle}</h1>
-  //   <h3>${todoDate}</h3>
-  //   <p></p>
-  // `;
+    todoList.appendChild(todoElement);
+    
+    const titleBoxElement = document.createElement('div');
+    titleBoxElement.classList.add('title_box');
+    todoElement.appendChild(titleBoxElement);
+    
+    const titleElement = document.createElement('h3');
+    titleElement.classList.add('title');
+    titleElement.textContent =`${todo.title}`;
+    titleBoxElement.appendChild(titleElement);
+    
+    const timeElement = document.createElement('span');
+    timeElement.classList.add('time');
+    timeElement.textContent =`(${todo.date})`;
+    titleBoxElement.appendChild(timeElement);
+    
+    const todoDiscrElement = document.createElement('p');
+    todoDiscrElement.textContent =`${todo.discription}`;
+    todoElement.appendChild(todoDiscrElement);
+
+    const deleteBtnElement = document.createElement('button');
+    deleteBtnElement.classList.add('delete_Btn')
+    deleteBtnElement.textContent = 'Delete';
+    todoElement.appendChild(deleteBtnElement);
+    
+    console.log(todoElement);
+  });
+
+  todoTitleText.value = '';
+  todoDateText.value = '';
+  todoDiscriptionText.value = '';
+  
+  deleteBtn = document.querySelector('.delete_Btn')
+  todoBox = document.querySelector('.todo_box')
 }
 
+// console.log(todoBox);
+// function deleteTodoBox() {
+// }
+
+deleteBtn.addEventListener('click', (e) => {
+  // todoBox.outerHTML = '';
+  console.log(deleteBtn);
+  console.log(e.srcElement.parentNode);
+  todoList.removeChild(e.srcElement.parentNode)
+}, true);
