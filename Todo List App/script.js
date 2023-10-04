6fbba2114d66b459d32531cfd1474b3f917ff137
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-10-04 20:40:17 +0530

    feat: add task done feature

--- Todo List App/script.js
+++ Todo List App/script.js
@@ -8,84 +8,84 @@ let todoDiscriptionText = document.querySelector('#descr_input');
 const todoList = document.querySelector('.todo_list')
 // console.log(todoList);
 
-// let allTodos = [];
 
+//* addEventListener to addBtn
 addBtn.addEventListener('click' ,(e)=> {
   let todoObject = {};
   // console.log(todoTitle);
   // console.log(todoDate);
 
   if (todoTitleText.value === '') {
-    alert('Please enter Todo Title...!!')
+    alert('Please enter Task Title...!!')
   } else if (todoDateText.value === '') {
-    alert('Please select Todo Date-Time...!!')
+    alert('Please select Task Date...!!')
   } else {
     todoObject.title = `${todoTitleText.value}`;
     todoObject.date = `${todoDateText.value}`;
     todoObject.discription = `${todoDiscriptionText.value}`;
     // console.log(todoObject);
 
-    // allTodos.push(todoObject);
-    // console.log(allTodos);
   
-    createTodo(todoObject);
+    createTask(todoObject);
   }
   
 })
 
-// let deleteBtn = document.querySelector('.delete_Btn');
-// let todoBox = document.querySelector('.todo_box');
-// function create todo in todo_list:
-function createTodo(todo) {
-  // todoList.innerHTML = '';
-  // allTodos.forEach((todo) => {
-    const todoElement = document.createElement('div');
-    todoElement.classList.add('todo_box');
-    todoList.appendChild(todoElement);
-    
-    const titleBoxElement = document.createElement('div');
-    titleBoxElement.classList.add('title_box');
-    todoElement.appendChild(titleBoxElement);
-    
-    const titleElement = document.createElement('h3');
-    titleElement.classList.add('title');
-    titleElement.textContent =`${todo.title}`;
-    titleBoxElement.appendChild(titleElement);
-    
-    const timeElement = document.createElement('span');
-    timeElement.classList.add('time');
-    timeElement.textContent =`(${todo.date})`;
-    titleBoxElement.appendChild(timeElement);
-    
-    const todoDiscrElement = document.createElement('p');
-    todoDiscrElement.textContent =`${todo.discription}`;
-    todoElement.appendChild(todoDiscrElement);
+//* function to create new task:
+function createTask(task) {
+  const todoElement = document.createElement('div');
+  todoElement.classList.add('todo_box');
+  todoList.appendChild(todoElement);
+  
+  const titleBoxElement = document.createElement('div');
+  titleBoxElement.classList.add('title_box');
+  todoElement.appendChild(titleBoxElement);
+  
+  const titleElement = document.createElement('h3');
+  titleElement.classList.add('title');
+  titleElement.textContent =`${task.title}`;
+  titleBoxElement.appendChild(titleElement);
+  
+  const timeElement = document.createElement('span');
+  timeElement.classList.add('time');
+  timeElement.textContent =`(${task.date})`;
+  titleBoxElement.appendChild(timeElement);
+  
+  const todoDiscrElement = document.createElement('p');
+  todoDiscrElement.textContent =`${task.discription}`;
+  todoElement.appendChild(todoDiscrElement);
 
-    const deleteBtnElement = document.createElement('button');
-    deleteBtnElement.classList.add('delete_Btn');
-    deleteBtnElement.setAttribute('onclick', 'deleteTodoBox()')
-    deleteBtnElement.textContent = 'X';
-    todoElement.appendChild(deleteBtnElement);
-    
-    console.log(todoElement);
-  // });
+  const deleteBtnElement = document.createElement('button');
+  deleteBtnElement.classList.add('delete_Btn');
+  deleteBtnElement.setAttribute('onclick', 'deleteTodoBox()')
+  deleteBtnElement.textContent = '\u00d7';
+  todoElement.appendChild(deleteBtnElement);
+  
+  // console.log(todoElement);
 
   todoTitleText.value = '';
   todoDateText.value = '';
   todoDiscriptionText.value = '';
   
 }
-// todoBox = document.querySelector('.todo_box')
-
-// console.log(todoBox);
-// function deleteTodoBox() {
-  // }
   
-  
-  function deleteTodoBox()  {
+//* function to delete particular task: 
+function deleteTodoBox()  {
   const deleteBtn = document.querySelector('.delete_Btn');
   const todoBox = deleteBtn.parentNode;
-  console.log(todoBox);
-  // console.log(e.srcElement.parentNode);
+  // console.log(todoBox);
   todoList.removeChild(todoBox)
 };
+
+//* function to mark task done
+function taskDone() {
+  const deleteBtn = document.querySelector('.delete_Btn');
+  const todoBox = deleteBtn.parentNode;
+  if (count === 0) {
+    todoBox.style = 'text-decoration: line-through; color: gray;'
+    count = 1;
+  } else {
+    todoBox.style = 'text-decoration: line-through; color: gray;'
+    count = 0;
+  }
+}
