a974c41b34b1f2075ab450dd7144cabe115bda71
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-11-14 11:55:55 +0530

    update to Digital clock

--- learn new skills/Promese in JS/script.js
+++ learn new skills/Promese in JS/script.js
@@ -24,7 +24,13 @@ function getData(params) {
   })
   .then((userData) => {
     console.log(userData, typeof userData);
-    document.write(`<img src="${userData.avatar_url}" style="padding: 10px; border-radius: 25px; width: 300px; height:450px; box-shadow: 0 0 10px seagreen">`);
+    document.write(`
+      <div style="display: flex; flex-direction: column; align-items: center; border-radius:10px; overflow: hidden;">
+        <img src="${userData.avatar_url}" style="padding: 10px; border-radius: 25px; width: 300px; height:450px; box-shadow: 0 0 10px seagreen"> 
+        <h2 style="text-align: center;">${userData.name}</h2>
+        <h3 style="text-align: center;">Public-Repositories: ${userData.public_repos}</h3>
+      </div>
+    `);
   })
   .catch((err) => {
     alert(err);
