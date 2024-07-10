e1b173e3bec1d6bf753598130e5dafdd0a7d6223
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-07-10 11:22:21 +0530

    fix: fixes a bug - unable to check mark and unmaark completed goal, and storing data to local storage

--- FocusOnToday/script.js
+++ FocusOnToday/script.js
@@ -2,24 +2,33 @@
 //* selector
 const notification = document.querySelector('.notification')
 const progressBar = document.querySelector('.progress-bar')
+const progressBarValue = document.querySelector('.progress-value')
 const alertMsg = document.querySelector('.alert-message')
-
-// const goalInput1 = document.querySelector('#goal1-input')
-// const goalInput2 = document.querySelector('#goal2-input')
-// const goalInput3 = document.querySelector('#goal3-input')
 const goalInputList = document.querySelectorAll('.goal-input')
-
-// const goalselectIcon1 = document.querySelector('.select-icon1')
-// const goalselectIcon2 = document.querySelector('.select-icon2')
-// const goalselectIcon3 = document.querySelector('.select-icon3')
 const goalselectIconsList = document.querySelectorAll('.select-icon')
-
-
 const bottomMessage = document.querySelector('.message')
+const goalsList = document.querySelectorAll('.goal')
 
 
 
 //* State
+const notificationText = [
+  "Raise the bar by completing your goals!",
+  "Well begun is half done!",
+  "Just a step away, keep going!",
+  "Whoa! You just completed all the goals.",
+  "Whoa! You just completed all the goals.",
+  "Whoa! You just completed all the goals."
+]
+const bottomMessageText = [
+  "Move one step ahead, today!",
+ "Keep Going, You\'re making great progress!",
+ "Keep Going, You\'re making great progress!",
+  "Great!, You\'ve made it.",
+  "Great!, You\'ve made it.",
+  "Great!, You\'ve made it."
+]
+
 let goals = []
 
 if(JSON.parse(window.localStorage.getItem('goals'))) {
@@ -28,43 +37,22 @@ if(JSON.parse(window.localStorage.getItem('goals'))) {
   goalInputList.forEach((input, index) => goals[index] = { goal: '', isCompleted: false})
 }
 
-// console.log(JSON.parse(window.localStorage.getItem('goals')));
 
-// console.log(goals.filter(goal => goal.isCompleted));
-
-goalInputList.forEach((input, index) => input.value = goals[index].goal)
+goalInputList.forEach((input, index) => input.value = goals[index].goal) //* set the value from the goals to the particular input element
 
 goals.forEach((goal, index) => {
   if(goal.isCompleted){
-    goalInputList[index].classList.add('completed')
-    goalselectIconsList[index].classList.add('completed')
+    //* add 'completed' class to the '.goal' elemenet, if particular goal is completed already.
+    goalsList[index].classList.add('completed')
   }
 })
 
 
-// progressBar.style.display = 'flex';
-
-if(goals[0]?.isCompleted && goals[1]?.isCompleted && goals[2]?.isCompleted) {
-  progressBar.style.width = '100%';
-  progressBar.textContent = '3/3 Completed';
-  notification.textContent = '"Whoa! You just completed all the goals, time for chill 😄"'
-  bottomMessage.textContent = '"Great!, You\'ve made it."'
-}
-else if((goals[0]?.isCompleted && goals[1]?.isCompleted)  || (goals[0]?.isCompleted && goals[2]?.isCompleted) || (goals[2]?.isCompleted && goals[1]?.isCompleted)) {
-  progressBar.style.width = '67%';
-  progressBar.textContent = '2/3 Completed';
-  notification.textContent = '"Just a step away, keep going!"'
-  bottomMessage.textContent = '"Keep Going, You\'re making great progress!"'
-}
-else if(goals[0]?.isCompleted || goals[1]?.isCompleted || goals[2]?.isCompleted) {
-  progressBar.style.width = '33%';
-  progressBar.textContent = '1/3 Completed';
-  notification.textContent = '"Well begun is half done!"'
-  bottomMessage.textContent = '"Keep Going, You\'re making great progress!"'
-}else {
-  progressBar.style.display = 'none';
-}
-
+let completedGoals = goals.filter(goal => goal.isCompleted).length
+progressBar.style.width = `${(completedGoals/goals.length)* 100}%`;
+progressBarValue.innerText = `${completedGoals}/${goals.length} Completed`;
+notification.textContent = `${notificationText[completedGoals]}`;
+bottomMessage.textContent = `"${bottomMessageText[completedGoals]}"`;
 
 
 
@@ -73,7 +61,6 @@ else if(goals[0]?.isCompleted || goals[1]?.isCompleted || goals[2]?.isCompleted)
 //* Handlers (Handler function)
 function handleInput(e) {
   alertMsg.style.display = 'none';
-  console.log(e.target);
 
   const inputIndex = [...goalInputList].indexOf(e.target)
   goals[inputIndex].goal = e.target.value
@@ -81,63 +68,27 @@ function handleInput(e) {
 }
 
 
-
 function handleSelection(e) {
-  // console.log(e.target);
-
   if(!([...goalInputList].every(input => input.value))) {
+    //*Displays 'error message if all the goals haven not been set.
+
+    alertMsg.innerText = `Please set all the ${goals.length} goals!`
     alertMsg.style.display = 'block';
+    return //* due to return key word => 'handleSelection' function returns from here and below code will not be executed.
   }  
 
-  //  if (e.target === goalselectIcon1) {
-  //   if (goals[0].isCompleted) {
-  //     goals[0].isCompleted = false
-  //   } else {
-  //     goals[0].isCompleted = true
-  //   }
-  //   // console.log('jay Shreee');
-  //   goalInput1.classList.toggle('completed');
-  //   // goalselectIcon1.classList.toggle('completed');
-  // }
-  // else if (e.target === goalselectIcon2) {
-  //   if (goals[1].isCompleted) {
-  //     goals[1].isCompleted = false
-  //   } else {
-  //     goals[1].isCompleted = true
-  //   }
-  //   goalInput2.classList.toggle('completed')
-  //   // goalselectIcon2.classList.toggle('completed');
-  // } else if (e.target === goalselectIcon3) {
-  //  if (goals[2].isCompleted) {
-  //     goals[2].isCompleted = false
-  //   } else {
-  //     goals[2].isCompleted = true
-  //   }
-  //   goalInput3.classList.toggle('completed')
-  //   // goalselectIcon3.classList.toggle('completed');
-  // }
-  const selectionIndex = [...goalselectIconsList].indexOf(e.target)
-  goalselectIconsList[selectionIndex].classList.toggle('completed')
-  e.target.classList.toggle('completed')
-  // console.log(goalselectIconsList[selectionIndex].classList);
-  // console.log(goalselectIconsList[selectionIndex].classList.toggle('completed'));
-  window.localStorage.setItem('goals', JSON.stringify(goals))
+  const selectionIndex = [...goalselectIconsList].indexOf(e.target) //* returns index of 'e.target' element.
+  goalsList[selectionIndex].classList.toggle('completed') //* toggles 'completed' to '.goal' element which is the parent of 'e.target' element.
+  goals[selectionIndex].isCompleted = !(goals[selectionIndex]?.isCompleted) //* toggles the value of 'goals[selectionIndex].isCompleted' (true to false, or vice versa)
+  window.localStorage.setItem('goals', JSON.stringify(goals)) //* save changes to local storage
   
-
-  
-  // progressBar.style.display = 'flex';
-
-  const completedGoals = goals.filter(goal => goal.isCompleted).length
-  // console.log(goals.length, completedGoals);
+  //* 
+  completedGoals = goals.filter(goal => goal.isCompleted).length
   progressBar.style.width = `${(completedGoals/goals.length)* 100}%`;
-  progressBar.textContent = `${completedGoals}/${goals.length} Completed`;
-  notification.textContent = '"Whoa! You just completed all the goals, time for chill."'
-  bottomMessage.textContent = '"Great!, You\'ve made it."'
-
-
+  progressBarValue.innerText = `${completedGoals}/${goals.length} Completed`;
+  notification.textContent = `${notificationText[completedGoals]}`;
+  bottomMessage.textContent = `"${bottomMessageText[completedGoals]}"`;
 }
-
-
 
 
 //* Events
